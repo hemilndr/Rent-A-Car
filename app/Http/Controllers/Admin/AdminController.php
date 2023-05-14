be8818b2b19ea7c40a749aaa2f3adedb8b1ab74a
hemil <hemilprasoon@gmail.com>
2023-05-14 19:33:37 +0530

    car-rental

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function login()
+    {
+
+        return view('admin.login');
+    }
+    public function adminLogin(Request $request)
+    {
+        $validated = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6|max:20',
+        ]);
+        $input = ['email' => request('email'), 'password' => request('password')];
+        if (auth()->guard('web')->attempt($input)) {
+            // return redirect(route('admin.dashboard'));
+            return view('admin.dashboard');
+        } else {
+            return back()->with('message', 'Invalid Login, Please try to login again');
+        }
+    }
+    public function dashboard(Request $request)
+    {
+        return view('admin.dashboard');
+    }
+    public function logout()
+    {
+        auth()->guard('users')->logout();
+
+        return redirect()->route('admin.login.form')->with('message', 'Succesfully Logged out');
+    }
+}
